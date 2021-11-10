81fe5bb14d954515e39b4f01824f9bd47adc4d22
Jan Šfaffa <agenttupesy@gmail.com>
2021-11-10 20:16:20 +0100

    added graph zooming + updated ui

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,24 +1,37 @@
-import { useEffect, useRef, useState } from "react";
-import { Graph } from "../graph";
-import { getObjectFitSize } from "../utils/getObjectFitSize";
-import { randomRGBColor } from "../utils/random";
-import { FaTrashAlt } from "react-icons/fa";
+import { useEffect, useRef, useState } from 'react';
+import { FaChevronLeft, FaChevronRight, FaTrashAlt } from 'react-icons/fa';
+import { IoMdAddCircle } from 'react-icons/io';
+import { Graph } from '../graph';
+import { getObjectFitSize } from '../utils/getObjectFitSize';
+import { randomRGBColor } from '../utils/random';
 export interface CanvasProps {}
 
 interface GraphInput {
   expression: string;
   color: string;
 }
+
+const ZOOM_STEP = 3;
+const MAX_ZOOM = {
+  MAX: 200,
+  MIN: 10,
+};
 const Canvas: React.FC<CanvasProps> = () => {
   const canvas = useRef<HTMLCanvasElement>(null);
-  const [currentExpression, setCurrentExpression] = useState<string>("");
   const graph = useRef<Graph>();
   const [graphInputs, setGraphInputs] = useState<GraphInput[]>([
-    { expression: "x", color: "#f00" },
-    { expression: "", color: "#00f" },
+    { expression: 'x', color: randomRGBColor() },
   ]);
   const graphInputsRef = useRef<GraphInput[]>(graphInputs);
   graphInputsRef.current = graphInputs;
+
+  const addGraphRef = useRef<HTMLDivElement>(null);
+
+  const [toolboxOpen, setToolBoxOpen] = useState<boolean>(true);
+
+  const [graphDetail, setGraphDetail] = useState<number>(15);
+  const graphDetailRef = useRef<number>(graphDetail);
+  graphDetailRef.current = graphDetail;
 
   useEffect(() => {
     if (!canvas.current) return;
@@ -32,94 +45,148 @@ const Canvas: React.FC<CanvasProps> = () => {
 
     canvas.current.width = dimensions.width;
     canvas.current.height = dimensions.height;
-    graph.current = new Graph(canvas.current, 20);
+    graph.current = new Graph(canvas.current);
 
     graph.current.initialize();
 
     let dragging = false;
-    canvas.current.addEventListener("mousedown", (e) => {
+    canvas.current.addEventListener('mousedown', (e) => {
       dragging = true;
     });
 
     const stopDrag = () => {
       dragging = false;
-      canvas.current?.classList.remove("dragging");
+      canvas.current?.classList.remove('dragging');
     };
-    canvas.current.addEventListener("mouseup", stopDrag);
-    canvas.current.addEventListener("mouseleave", stopDrag);
+    canvas.current.addEventListener('mouseup', stopDrag);
+    canvas.current.addEventListener('mouseleave', stopDrag);
 
-    canvas.current.addEventListener("mousemove", (e) => {
+    canvas.current.addEventListener('mousemove', (e) => {
       if (!dragging || !graph.current) return;
 
-      canvas.current?.classList.add("dragging");
-      graph.current.moveGraph(e.movementX, e.movementY);
+      canvas.current?.classList.add('dragging');
+      graph.current.transformGraph({
+        xDelta: e.movementX,
+        yDelta: e.movementY,
+        points: graphDetailRef.current,
+      });
     });
-    canvas.current.addEventListener("wheel", () => {
-      console.log("scrolling");
+    canvas.current.addEventListener('wheel', (e: WheelEvent) => {
+      if (!graph.current) return;
+      let zoomDelta = 0;
+      if (e.deltaY < 0) {
+        if (graph.current.squareSize + ZOOM_STEP > MAX_ZOOM.MAX) return;
+        zoomDelta = ZOOM_STEP;
+      } else {
+        if (graph.current.squareSize - ZOOM_STEP < MAX_ZOOM.MIN) return;
+        zoomDelta = -ZOOM_STEP;
+      }
+      graph.current.transformGraph({
+        xDelta: 0,
+        yDelta: 0,
+        zoomDelta: zoomDelta,
+        points: graphDetailRef.current,
+      });
     });
-    //  canvas.current.addEventListener('mousewheel', (e: WheelEvent) => {
-    //    // e.preventDefault();
-    //    // if (e.wheelDelta < 0) {
-    //    //   unzoom();
-    //    // } else {
-    //    //   zoom();
-    //    // }
-    //  });
   }, []);
 
   useEffect(() => {
     if (!graph.current) return;
     graph.current.clearGraph();
-    for (const input of graphInputs) {
-      graph.current.drawGraph(input.expression, input.color);
+    for (const { expression, color } of graphInputs) {
+      graph.current.drawGraph(expression, color, graphDetail);
     }
-  }, [graphInputs]);
+
+    if (graphInputs.filter((graph) => graph.expression === '').length > 0) {
+      addGraphRef.current?.classList.add('banned');
+    } else {
+      addGraphRef.current?.classList.remove('banned');
+    }
+  }, [graphInputs, graphDetail]);
+
   const addGraph = () => {
-    console.log("called");
+    if (
+      graphInputsRef.current.filter((graph) => graph.expression === '').length >
+      0
+    )
+      return;
     const newGraphInput: GraphInput = {
       color: randomRGBColor(),
-      expression: "",
+      expression: '',
     };
     setGraphInputs([...graphInputsRef.current, newGraphInput]);
   };
+
+  // TODO: store graph data in URL
   return (
     <div>
-      <div className="toolbox">
+      <div
+        className="toolbox-control"
+        onClick={() => setToolBoxOpen(!toolboxOpen)}
+      >
+        {toolboxOpen ? <FaChevronLeft /> : <FaChevronRight />}
+      </div>
+      <div
+        className="toolbox"
+        style={{ visibility: toolboxOpen ? 'visible' : 'hidden' }}
+      >
+        {/* <h1>Graph-cal</h1> */}
         {graphInputs.map((graphInput, idx) => (
-          <div className="expression-input" key={idx}>
+          <div className="graph-input" key={idx}>
             <div
               className="color-code"
               style={{ backgroundColor: graphInput.color }}
             ></div>
-            <label>f(x) = </label>
+            <div className="graph-info">
+              <span>f(x)</span> =
+            </div>
             <input
               type="text"
               value={graphInput.expression}
               onChange={(e) => {
                 const updatedGraphInputs = [...graphInputs];
                 const thisGraph = updatedGraphInputs[idx];
-                const newGraphInput: GraphInput = {
-                  color: randomRGBColor(),
-                  expression: "",
-                };
                 updatedGraphInputs[idx] = {
                   ...thisGraph,
                   expression: e.target.value,
                 };
-                if (idx === graphInputs.length - 1) {
-                  if (thisGraph.expression === "" && e.target.value !== "") {
-                    updatedGraphInputs.push(newGraphInput);
-                  }
-                }
 
                 setGraphInputs(updatedGraphInputs);
               }}
             />
-            <div className="remove">
+            <div
+              className="remove"
+              onClick={() => {
+                const updatedGraphInputs = [...graphInputs];
+                updatedGraphInputs.splice(idx, 1);
+
+                setGraphInputs(updatedGraphInputs);
+              }}
+            >
               <FaTrashAlt />
             </div>
           </div>
         ))}
+        <div className="add-graph" onClick={addGraph} ref={addGraphRef}>
+          <IoMdAddCircle />
+        </div>
+        <hr />
+        <div className="toolbox-options">
+          <div>
+            <b>Options:</b>
+          </div>
+          <div>
+            <label>detail: </label>
+            <input
+              type="range"
+              min={1}
+              max={20}
+              value={graphDetail}
+              onChange={(e) => setGraphDetail(parseInt(e.target.value))}
+            />
+            {graphDetail}
+          </div>
+        </div>
       </div>
       <canvas id="graph" ref={canvas}></canvas>;
     </div>
--- src/graph.ts
+++ src/graph.ts
@@ -1,8 +1,8 @@
-import { Parser } from "expr-eval";
+import { Parser } from 'expr-eval';
 
-const SQUARE_BORDER_COLOR = "#000";
-const AXIS_COLOR = "#666";
-const DEFAULT_POINTS_PER_SQUARE = 5;
+const SQUARE_BORDER_COLOR = '#000';
+const AXIS_COLOR = '#666';
+const DEFAULT_POINTS_PER_SQUARE = 20;
 
 export interface Point {
   x: number;
@@ -15,8 +15,6 @@ export interface GraphFunction {
 }
 export class Graph {
   private ctx: CanvasRenderingContext2D | null = null;
-  // TODO: remove squaresInQuadron vvv
-  readonly squaresInQuadron: number = 0;
   private _squareSize: number = 42;
   private center: Point | null = null;
   private quadrons: { width: number; height: number }[] | null = [];
@@ -25,11 +23,10 @@ export class Graph {
   get squareSize() {
     return this._squareSize;
   }
-  constructor(canvas: HTMLCanvasElement, squaresInQuadron: number = 10) {
-    const ctx = canvas.getContext("2d");
+  constructor(canvas: HTMLCanvasElement) {
+    const ctx = canvas.getContext('2d');
     ctx?.translate(0.5, 0.5);
     this.ctx = ctx;
-    this.squaresInQuadron = squaresInQuadron;
   }
 
   initialize = () => {
@@ -60,30 +57,20 @@ export class Graph {
 
     return [
       {
-        width: centerY > 0 && centerX > 0 ? quadronWidths.negative : 0,
-        height: centerY > 0 && centerX > 0 ? quadronHeights.positive : 0,
+        width: centerX > 0 ? quadronWidths.negative : 0,
+        height: centerY > 0 ? quadronHeights.positive : 0,
       },
       {
-        width:
-          centerY > 0 && centerX < canvasWidth ? quadronWidths.positive : 0,
-        height:
-          centerY > 0 && centerX < canvasWidth ? quadronHeights.positive : 0,
+        width: centerX < canvasWidth ? quadronWidths.positive : 0,
+        height: centerY > 0 ? quadronHeights.positive : 0,
       },
       {
-        width:
-          centerX > 0 && centerY < canvasHeight ? quadronWidths.negative : 0,
-        height:
-          centerX > 0 && centerY < canvasHeight ? quadronHeights.negative : 0,
+        width: centerX > 0 ? quadronWidths.negative : 0,
+        height: centerY < canvasHeight ? quadronHeights.negative : 0,
       },
       {
-        width:
-          centerX < canvasWidth && centerY < canvasHeight
-            ? quadronWidths.positive
-            : 0,
-        height:
-          centerX < canvasWidth && centerY < canvasHeight
-            ? quadronHeights.negative
-            : 0,
+        width: centerX < canvasWidth ? quadronWidths.positive : 0,
+        height: centerY < canvasHeight ? quadronHeights.negative : 0,
       },
     ];
   };
@@ -148,18 +135,16 @@ export class Graph {
     this.ctx.lineTo(center.x, this.ctx.canvas.height);
     this.ctx.stroke();
 
-    this.ctx.font = "12px sans-serif";
-    this.ctx.fillStyle = "#999";
+    this.ctx.font = '12px sans-serif';
+    this.ctx.fillStyle = '#999';
     for (let i = -this.quadrons[0].width; i < this.quadrons[1].width; i++) {
       const realPos = this.center.x + i * this.squareSize;
       this.ctx.fillText(i.toString(), realPos + 5, center.y + 15);
     }
-    for (let i = -this.quadrons[0].height; i < this.quadrons[3].height; i++) {
+    for (let i = -this.quadrons[3].height; i < this.quadrons[0].height; i++) {
       if (i === 0) continue;
-      const realPos = this.center.y + i * this.squareSize;
+      const realPos = this.center.y - i * this.squareSize;
       this.ctx.fillText(i.toString(), center.x + 5, realPos + 15);
-      //this.ctx.fillRect(realPos, center.y - 5, 1.5, 10);
-      //this.ctx.fillRect(center.x - 5, realPos, 10, 1.5);
     }
     return true;
   };
@@ -181,17 +166,23 @@ export class Graph {
   private getPointsFromExpression = (expression: string, points: number) => {
     if (!this.quadrons) return null;
     const functionData: Point[] = [];
-    for (let i = -this.quadrons[0].width; i < this.quadrons[1].width; i++) {
+    const negativeWidth = Math.max(
+      this.quadrons[0].width,
+      this.quadrons[2].width
+    );
+    const positiveWidth = Math.max(
+      this.quadrons[1].width,
+      this.quadrons[3].width
+    );
+    for (let i = -negativeWidth; i < positiveWidth; i++) {
       const step = 1 / points;
       for (let j = 0; j < 1; j += step) {
         const x = i + j;
         // f(x) = x
         try {
           const y = Parser.evaluate(expression, { x });
-          if (y < this.squareSize * (this.squaresInQuadron * 2)) {
-            // y: -y to invert x axis
-            functionData.push({ x, y: -y });
-          }
+          // y: -y to invert x axis
+          functionData.push({ x, y: -y });
         } catch (e) {}
       }
     }
@@ -216,19 +207,31 @@ export class Graph {
     this.initialize();
     return true;
   };
-  moveGraph = (x: number, y: number) => {
+  transformGraph = ({
+    xDelta,
+    yDelta,
+    zoomDelta = 0,
+    points = DEFAULT_POINTS_PER_SQUARE,
+  }: {
+    xDelta: number;
+    yDelta: number;
+    zoomDelta?: number;
+    points?: number;
+  }) => {
     if (!this.ctx || !this.center) return false;
     this.center = {
-      x: this.center.x + x,
-      y: this.center.y + y,
+      x: this.center.x + xDelta,
+      y: this.center.y + yDelta,
     };
+
+    this._squareSize = this.squareSize + zoomDelta;
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     this.drawGrid(this.center);
     this.drawAxis(this.center);
     for (const graph of this.graphs) {
       const functionData = this.getPointsFromExpression(
         graph.expression,
-        DEFAULT_POINTS_PER_SQUARE
+        points
       );
       if (!functionData) return false;
       const updatedGraph: GraphFunction = {
